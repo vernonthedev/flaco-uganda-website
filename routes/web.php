f69b18f38cb778700a1439009c2f64026f6ae4b2
vernonthedev <techjaja2@gmail.com>
2023-12-11 03:41:45 -0800

    basic pages setup

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('home.index');
-});
+// the website routes
+Route::get('/', function () { return view('home.index'); })->name('home');
+Route::get('/contacts', function () { return view('home.contact'); })->name('contact');
+Route::get('/about-us', function () { return view('home.about'); })->name('about');
+Route::get('/gallery', function () { return view('home.gallery'); })->name('gallery');
+Route::get('/causes', function () { return view('home.causes'); })->name('causes');
+Route::get('/blog', function () { return view('home.blog'); })->name('blog');
