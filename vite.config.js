d781a6ebfad78c11a167a1dca86fe5a5661a62d0
vernonthedev <techjaja2@gmail.com>
2023-12-11 02:48:43 -0800

    setup home page layout

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,30 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                // references to the css files
+                'resources/css/bootstrap/bootstrap-grid.css',
+                'resources/css/bootstrap/bootstrap-reboot.css',
+                'resources/css/css/mixins/_text-hide.css',
+                'resources/css/css/bootstrap-reboot.css',
+                'resources/css/animate.css',
+                'resources/css/aos.css',
+                'resources/css/bootstrap-datepicker.css',
+                'resources/css/bootstrap.min.css',
+                'resources/css/flaticon.css',
+                'resources/css/icomoon.css',
+                'resources/css/ionicons.min.css',
+                'resources/css/jquery.timepicker.css',
+                'resources/css/magnific-popup.css',
+                'resources/css/open-iconic-bootstrap.min.css',
+                'resources/css/owl.carousel.min.css',
+                'resources/css/owl.theme.default.min.css',
+                'resources/css/style.css',
+                'resources/css/ajax-loader.gif',
+
+
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
